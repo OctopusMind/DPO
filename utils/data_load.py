10154660c562dd428afd7945e2d63570724b6347
algorithmexplorer <151371931+algorithmexplorer@users.noreply.github.com>
2024-06-12 17:16:15 +0800

    Update data_load.py

--- utils/data_load.py
+++ utils/data_load.py
@@ -38,37 +38,28 @@ class CustomDataset(Dataset):
         return [prompt_inputs, rejected_inputs, chosen_inputs]
 
     def collate_fn(self, batch):
-        def statisc_ids(data, masks=None, labels=None, is_pad=False):
+        def statisc_ids(data, labels_mask):
             max_length = max([len(i) for i in data])
             attention_masks = []
             return_ids = []
-            return_labels = []
-            if is_pad:
-                for one, mask, label in zip(data, masks, labels):
-                    padding_num = max_length - len(one)
-                    return_ids.append(one + [self.tokenizer.pad_token_id] * padding_num)
-                    return_labels.append(label + [0] * padding_num)
-                    attention_masks.append(mask + [0] * padding_num)
-            else:
-                for one in data:
-                    padding_num = max_length - len(one)
-                    return_ids.append([self.tokenizer.bos_token_id] * padding_num + one)
-                    attention_masks.append([0] * padding_num + [1] * len(one))
-            return return_ids, attention_masks, return_labels
+            labels_masks = []
+            for one, mask in zip(data, labels_mask):
+                padding_num = max_length - len(one)
+                return_ids.append(one + [self.tokenizer.pad_token_id] * padding_num)
+                labels_masks.append(mask + [0] * padding_num)
+                attention_masks.append([1] * len(one) + [0] * padding_num)
+            return return_ids, attention_masks, labels_masks
 
-        prompt_inputs, prompt_masks, _ = statisc_ids([one[0] for one in batch])
         inputs_ids = []
-        inputs_masks = []
-        labels = []
+        labels_mask = []
         for i in range(1, 3):
-            for one, prompt_input, prompt_mask in zip(batch, prompt_inputs, prompt_masks):
+            for one, prompt_input in zip(batch, [one[0] for one in batch]):
                 res = one[i]
                 inputs_ids.append(prompt_input + res)
-                inputs_masks.append(prompt_mask + [1] * len(res))
-                labels.append([0] * len(prompt_input) + [1] * len(res))
-        inputs_ids, inputs_masks, labels_mask = statisc_ids(inputs_ids, inputs_masks, labels, True)
+                labels_mask.append([0] * len(prompt_input) + [1] * len(res))
+        inputs_ids, inputs_masks, labels_masks = statisc_ids(inputs_ids, labels_mask)
         return {"inputs_ids": torch.tensor(inputs_ids), "inputs_masks": torch.tensor(inputs_masks),
-                "labels_mask": torch.tensor(labels_mask)}
+                "labels_mask": torch.tensor(labels_masks)}
 
 
 if __name__ == '__main__':
